2585354e52185814b2de0c0ce7a36c5babf8134c
Haisheng.Wu <freizl@gmail.com>
2014-12-17 07:55:36 -0800

    it has before/after beforeEach/afterEach as normal TDD framework.

--- tests/unit/hello.js
+++ tests/unit/hello.js
@@ -6,6 +6,22 @@ define([
 
 	tdd.suite('Say hello', function () {
 
+		tdd.before(function () {
+			console.log(" 111 : before test once");
+		})
+
+		tdd.after(function () {
+			console.log(" 999 : after test once");
+		})
+
+		tdd.beforeEach(function () {
+			console.log(" 333 : before each test");
+		})
+
+		tdd.afterEach(function () {
+			console.log(" 555 : after each test ");
+		})
+
 		tdd.test('greeting', function () {
 			var expected = hello.greet('Beanstock Media');
 			assert.strictEqual(expected, 'Hello, Beanstock Media!', 'shall be able to greet')
