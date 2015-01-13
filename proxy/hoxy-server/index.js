1c96659174c8a4d3fc0f373d36b7678903574e45
Haisheng.Wu <freizl@gmail.com>
2015-01-13 14:42:59 -0800

    be able to add and remove mocks.

--- proxy/hoxy-server/index.js
+++ proxy/hoxy-server/index.js
@@ -1,13 +1,12 @@
-var connect = require('connect');
-var http = require('http')
-var app = connect()
-
-
-var hoxy = require('hoxy'),
+var connect = require('connect'),
+	http = require('http'),
+	app = connect(),
+	R = require('ramda'),
+	url = require('url'),
+	hoxy = require('hoxy'),
 	fs = require('fs'),
 	port = 9900,     // TODO: may be pass from command line arguments
-	proxy = new hoxy.Proxy().listen(port)
-	;
+	proxy = new hoxy.Proxy().listen(port);
 
 // ====================
 // APN
@@ -20,11 +19,8 @@ proxy.intercept({
 	host: 'ib.adnxs.com'
 }, function(req, resp){
 
-	var url = req.url,
-		xs = url.split('&'),
-		ys = xs.filter(function (x) { return x.indexOf('callback') === 0;}),
-		data
-		;
+	var url_parts = url.parse(req.url, true),
+		data;
 
 	console.log('[' + new Date() + ' ] Intercept adnxs.com: ' + req.url);
 
@@ -41,41 +37,95 @@ proxy.intercept({
 // Criteo
 // ====================
 
-proxy.intercept({
-	phase: 'request',
-	method: 'GET',
-	as: 'string',
-	host: 'rtax.criteo.com'
-}, function(req, resp){
-
-	console.log('[' + new Date() + ' ] Intercept rtax.criteo.com: ' + req.url);
-
-	resp.headers['content-type'] = 'text/javascript'
-	resp.string = "crtg_content = 'criteo300=1'; (function(){document.cookie = 'crtg_rta=' + escape(crtg_content) + '; path=/; expires=Wed, 11 Feb 2016 19:02:31 GMT; domain=localhost';})();"
-});
 
 proxy.intercept({
 	phase: 'request',
 	method: 'GET',
-	as: 'string',
 	host: 'cas.criteo.com'
-}, function(req, resp){
+}, function(req, resp, done){
+
+	var url_parts = url.parse(req.url, true),
+		query = url_parts.query,
+		file = '/data/criteo_' + query.zoneid + '.js'
 
 	console.log('[' + new Date() + ' ] Intercept cas.criteo.com: ' + req.url);
 
-	var data = fs.readFileSync('data/criteo.js');
-	//console.log(data);
-	resp.headers['content-type'] = 'text/javascript'
-	resp.headers['by-proxy'] = 'hoxy'
-	resp.string = data
+	this.serve(__dirname + file, done)
 });
 
 // ====================
 // http server
 // ====================
 
-app.use(function(req, res){
-	// TODO: Enable / Disable new intercept rules
-	res.end('Hello from Connect!\n');
+var handlers = {};
+
+function getInterceptHandler (phase) {
+	var xs = proxy._intercepts[phase] || [],
+		index = xs.length - 1
+	return xs[index]
+}
+
+function removeInterceptHandler (phase, name) {
+	var xs = proxy._intercepts[phase] || [];
+
+	if (!R.isEmpty(xs) && !R.isEmpty(handlers[name])) {
+		proxy._intercepts[phase] = R.filter(function (it) {
+			return it !== handlers[name]
+		})(xs)
+	}
+}
+
+function addMock (name, opts, handler) {
+
+	if (name) {
+		proxy.intercept(opts, handler)
+		handlers[name] = getInterceptHandler('request')
+		console.log(opts);
+		console.log(handler);
+		console.log(handlers[name]);
+		return 'add intercept success for: ' + name
+	} else {
+		return 'NO intercept added without specify a name'
+	}
+}
+
+
+app.use('/mockCriteo', function(req, res, next){
+	var url_parts = url.parse(req.url, true),
+		query = url_parts.query,
+		name = query.name,
+		content = query.content || '', // sample: criteo300=1;criteo728=1;criteo160=1;
+		result = addMock(name, {
+			phase: 'request',
+			method: 'GET',
+			as: 'string',
+			host: 'rtax.criteo.com'
+		}, function (req, resp){
+
+			console.log('[' + new Date() + ' ] Intercept rtax.criteo.com: ' + req.url);
+
+			resp.headers['content-type'] = 'text/javascript'
+			resp.string = "crtg_content = '" + content + "'; (function(){document.cookie = 'crtg_rta=' + escape(crtg_content) + '; path=/; expires=Wed, 11 Feb 2016 19:02:31 GMT; domain=localhost';})();"
+		});
+
+	res.end(result)
+	next();
 })
+app.use('/removeMock', function(req, res, next){
+
+	var url_parts = url.parse(req.url, true),
+		query = url_parts.query,
+		name = query.name,
+		phase = query.phase || 'request'
+
+	if (!!query.name && handlers[name]) {
+		removeInterceptHandler(phase, name)
+		res.end('intercepts have been removed: ' + name)
+	} else {
+		res.end('nothing will be removed for ' + name)
+	}
+
+	next();
+})
+
 http.createServer(app).listen(3000)
